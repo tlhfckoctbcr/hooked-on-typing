efbe1042d600f4979fa1a436eeedee65a45dc910
kingpoststudio <stu@kingpoststudio.com>
2019-01-30 00:26:57 -0500

    Playing with hooks and react-spring

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React from "react";
-import "./App.css";
-import Table from "./components/Table/Table";
+import React from 'react'
+import { useTrail, animated } from 'react-spring/hooks'
+import './App.css'
 
-const App = () => (
-  <Table
-    title={"Table Title"}
-    columns={[
-      { title: "Name", field: "name" },
-      { title: "Age", field: "age" },
-      { title: "Phone", field: "phone" }
-    ]}
-    data={[
-      {
-        name: "Stuart",
-        age: 30,
-        phone: "843-123-4500"
-      },
-      {
-        name: "Ally",
-        age: 26,
-        phone: "843-123-4412"
-      }
-    ]}
-  />
-);
+const fast = { tension: 1200, friction: 40 }
+const slow = { mass: 10, tension: 200, friction: 50 }
+const trans = (x, y) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
 
-export default App;
+export default function App() {
+  const [trail, set] = useTrail(3, () => ({ xy: [0, 0], config: i => (i === 0 ? fast : slow) }))
+  return (
+    <>
+      <svg style={{ position: 'absolute', width: 0, height: 0 }}>
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
+          <feColorMatrix in="blur" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7" />
+        </filter>
+      </svg>
+      <div className="hooks-main" onMouseMove={e => set({ xy: [e.clientX, e.clientY] })}>
+        {trail.map((props, index) => (
+          <animated.div key={index} style={{ transform: props.xy.interpolate(trans) }} />
+        ))}
+      </div>
+    </>
+  )
+}
